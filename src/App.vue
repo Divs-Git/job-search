0617e32abb576c9ee1d1624350677d8723038261
Divs-Git <divyansh.sri258@gmail.com>
2024-02-02 09:03:44 +0530

    setting up initail files

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,3 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div class="wrapper">
-    <HelloWorld msg="Congratulations ! You are hired with LPA of 67 LPA" />
-  </div>
+  <h1>Hello Vue</h1>
 </template>
